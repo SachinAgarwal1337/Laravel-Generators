ef78fb7a68d79d81a7959ad64b359f5846e06d2b
Sachin Agarwal <sachinkumaragarwal05@gmail.com>
2015-05-28 15:38:36 +0530

    Add Project files
    Adding already developed project files. Tweaking should be done in next
    commit.

---
+++ src/Commands/ModelGeneratorCommand.php
@@ -0,0 +1,69 @@
+<?php namespace CMH\Console\Commands\ModelGenerator\Commands;
+
+use CMH\Console\Commands\ModelGenerator\GenerateClasses;
+use Illuminate\Console\Command;
+
+class ModelGeneratorCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'cmh:model {model} {--migration}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate Directory structure for a model';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @param GenerateClasses $class
+     *
+     * @return mixed
+     */
+    public function handle(GenerateClasses $class)
+    {
+        $model = ucfirst($this->argument('model'));
+        $migration = $this->option('migration');
+        $class->generate($model, $migration);
+
+        $this->info('Created Following Structure:');
+        $this->info("{$model}/");
+        $this->info("  |");
+        $this->info("  |_{$model}.php");
+        $this->info("  |");
+        $this->info("  |_Contracts/");
+        $this->info("    |");
+        $this->info("    |_{$model}Repository.php");
+        $this->info("  |");
+        $this->info("  |_Events/");
+        $this->info("  |");
+        $this->info("  |_Jobs/");
+        $this->info("  |");
+        $this->info("  |_Listeners/");
+        $this->info("  |");
+        $this->info("  |_Repositories/");
+        $this->info("    |");
+        $this->info("    |_Eloquent{$model}Repository.php");
+
+        if ($migration) {
+            $this->info("\n Migration for {$model} created");
+        }
+
+    }
+}
---
+++ src/GeneratorsServiceProvider.php
@@ -0,0 +1,26 @@
+<?php namespace CMH\Console\Commands\ModelGenerator;
+
+use Illuminate\Support\ServiceProvider;
+
+class GeneratorsServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
